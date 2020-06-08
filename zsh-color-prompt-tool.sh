b55ceedaa63980c99ffafd2760886b8bca9643dd
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2020-06-08 14:51:44 -0400

    Add files via upload

--- zsh-color-prompt-tool.sh
+++ zsh-color-prompt-tool.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo Hello World
+BOLD="$(tput bold)"
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+BLUE='\033[1;34m'
+PURPLE='\033[1;35m'
+CYAN='\033[1;36m'
+# Reset Color
+RC='\033[0m'
+echo -e "${BOLD}\nWelcome to the Zsh ${GREEN}C${RED}o${BLUE}l${PURPLE}o${CYAN}r ${RC}Prompt Tool\n${NORMAL}"
