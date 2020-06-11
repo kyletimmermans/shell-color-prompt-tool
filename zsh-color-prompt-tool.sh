25f0caab8ce60eb0b69bb88cf92f6d6720e7984c
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2020-06-11 14:07:46 -0400

    Add files via upload

--- zsh-color-prompt-tool.sh
+++ zsh-color-prompt-tool.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+# Title Colors / Format Vars
 BOLD="$(tput bold)"
 NORMAL="$(tput sgr0)"
 GREEN='\033[1;32m'
@@ -8,24 +9,24 @@ BLUE='\033[1;34m'
 PURPLE='\033[1;35m'
 CYAN='\033[1;36m'
 RC='\033[0m'  # Reset Color
-echo -e "${BOLD}\n~Welcome to the Zsh ${GREEN}C${RED}o${BLUE}l${PURPLE}o${CYAN}r ${RC}${BOLD}Prompt Tool~${NORMAL}~"
-echo "@KyleTimmermans\n"
+echo -e "${BOLD}\n                ~Welcome to the Zsh ${GREEN}C${RED}o${BLUE}l${PURPLE}o${CYAN}r ${RC}${BOLD}Prompt Tool~${NORMAL}"
+echo "                     @KyleTimmermans\n"
 echo "Enter the numbers in order of what you want your prompt to consist of:"
-echo "01. Username     02. Hostname (Short)     03. Hostname (Full)\n
-04. Return Status of Last Command    05. Current Working Directory\n
-06. Current Working Directory from "$"HOME    07. Current History Event Number\n
-08. yy-mm-dd   09. mm-dd-yy   10. day-dd  11. 12-Hour, AM/PM  12. 24-Hour w/ Seconds\n
-13. @  14. #  15. !  16. $  17. %  18. *  19. &  20. -  21. _  22. :  23. ; 24. |  25. /  26. ""\ \n
-27. Custom Text\n"
+echo "1. Username     2. Hostname (Short)     3. Hostname (Full)  4. Shell's TTY\n
+5. isPrivileged?  6. Return Status of Last Command    7. Current Working Directory\n
+8. Current Working Directory from "$"HOME    9. Current History Event Number\n
+10. yy-mm-dd   11. mm-dd-yy   12. day-dd  13. 12-Hour, AM/PM  14. 24-Hour w/ Seconds\n
+15. @  16. #  17. !  18. $  19. %  20. *  21. &  22. -  23. _  24. :  25. ; 26. |  27. /  28. ""\ \n
+29. Custom Text\n"
 echo "Type 'n' or 'N' when you're finished"
-prompt_array = () # Append to this empty array
-repeat = 1
-while [repeat == 1]
-do
-  read -n2 -p "Please enter number choice: " CHOICE
-  if [$CHOICE == 'n' || $CHOICE == 'N']
-    repeat = 0  # Break while-loop
+prompt_array=() # Append to this empty array
+declare -i repeat=1
+while [ "$repeat" -eq 1 ] ; do
+  read "CHOICE?Please enter number choice: " \n
+  if [[ "$CHOICE" =~ ^[Nn]$ ]] ; then    # If choice is 'n' or 'N' (regex)
+    declare -i repeat=0  # Break while-loop
   else
     prompt_array+=($CHOICE)  # Append to array
   fi
 done
+echo "$prompt_array\n"
