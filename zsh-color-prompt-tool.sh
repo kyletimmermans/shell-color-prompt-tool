4bccdf008873ad2ac126f73264b2f02127045315
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2020-06-12 00:17:05 -0400

    Add files via upload

--- zsh-color-prompt-tool.sh
+++ zsh-color-prompt-tool.sh
@@ -13,16 +13,16 @@ echo -e "${BOLD}\n                ~Welcome to the Zsh ${GREEN}C${RED}o${BLUE}l${
 echo "                     @KyleTimmermans\n"
 echo "Enter the numbers in order of what you want your prompt to consist of:"
 echo "1. Username     2. Hostname (Short)     3. Hostname (Full)  4. Shell's TTY\n
-5. isPrivileged?  6. Return Status of Last Command    7. Current Working Directory\n
+5. isPrivileged?   6. Return Status of Last Command    7. Current Working Directory\n
 8. Current Working Directory from "$"HOME    9. Current History Event Number\n
 10. yy-mm-dd   11. mm-dd-yy   12. day-dd  13. 12-Hour, AM/PM  14. 24-Hour w/ Seconds\n
 15. @  16. #  17. !  18. $  19. %  20. *  21. &  22. -  23. _  24. :  25. ; 26. |  27. /  28. ""\ \n
-29. Custom Text\n"
+29. Space  30. Custom Text\n"
 echo "Type 'n' or 'N' when you're finished"
 prompt_array=() # Append to this empty array
 declare -i repeat=1
 while [ "$repeat" -eq 1 ] ; do
-  read "CHOICE?Please enter number choice: " \n
+  read "CHOICE?Enter number choice: " \n
   if [[ "$CHOICE" =~ ^[Nn]$ ]] ; then    # If choice is 'n' or 'N' (regex)
     declare -i repeat=0  # Break while-loop
   else
